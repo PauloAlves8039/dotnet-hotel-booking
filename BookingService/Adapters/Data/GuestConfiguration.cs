60b806b6956215a817cbc3628dd4bccccbe4eeea
PauloAlves8039 <pj38alves@gmail.com>
2023-02-12 14:32:19 -0300

    Aplicando o uso de ValueObjects

---
+++ BookingService/Adapters/Data/GuestConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Entities = Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Data
+{
+    public class GuestConfiguration : IEntityTypeConfiguration<Entities.Guest>
+    {
+        public void Configure(EntityTypeBuilder<Entities.Guest> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.OwnsOne(x => x.DocumentId)
+                .Property(x => x.IdNumber);
+
+            builder.OwnsOne(x => x.DocumentId)
+                .Property(x => x.DocumentType);
+        }
+    }
+}
