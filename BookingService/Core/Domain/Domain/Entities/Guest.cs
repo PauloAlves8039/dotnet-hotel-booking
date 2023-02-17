c9a9f5d3875cc9a7ac033ab98d27d9f871a6b8d5
PauloAlves8039 <pj38alves@gmail.com>
2023-02-17 17:57:02 -0300

    Criando os métodos Save e ValidateState para Guest

--- BookingService/Core/Domain/Domain/Entities/Guest.cs
+++ BookingService/Core/Domain/Domain/Entities/Guest.cs
@@ -1,4 +1,6 @@
-﻿using Domain.ValueObjects;
+﻿using Domain.Exceptions;
+using Domain.Ports;
+using Domain.ValueObjects;
 
 namespace Domain.Entities
 {
@@ -9,5 +11,36 @@ namespace Domain.Entities
         public string Surname { get; set; }
         public string Email { get; set; }
         public PersonId DocumentId { get; set; }
+
+        public async Task Save(IGuestRepository guestRepository)
+        {
+            this.ValidateState();
+
+            if (this.Id == 0)
+            {
+                this.Id = await guestRepository.Create(this);
+            }
+        }
+
+        private void ValidateState()
+        {
+            if (DocumentId == null ||
+               DocumentId.IdNumber.Length <= 3 ||
+               DocumentId.DocumentType == 0)
+            {
+                throw new InvalidPersonDocumentIdException();
+            }
+
+            if (Name == null || Surname == null || Email == null)
+            {
+                throw new MissingRequiredInformation();
+            }
+
+            if (Utils.ValidateEmail(this.Email) == false)
+            {
+                throw new InvalidEmailException();
+            }
+        }
+
     }
 }
