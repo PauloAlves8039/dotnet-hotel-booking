3ec7b92b4e85e4b065b3e0a8cc7712f9ee2a47b6
PauloAlves8039 <pj38alves@gmail.com>
2023-02-25 13:03:29 -0300

    Criando BookingController e o BookingManager

---
+++ BookingService/Core/Application/Application/Booking/BookingManager.cs
@@ -0,0 +1,30 @@
+﻿using Application.Booking.Dtos;
+using Application.Booking.Ports;
+using Domain.Booking.Ports;
+
+namespace Application.Booking
+{
+    public class BookingManager : IBookingManager
+    {
+        private readonly IBookingRepository _bookingRepository;
+
+        public BookingManager(IBookingRepository bookingRepository)
+        {
+            _bookingRepository = bookingRepository;
+        }
+
+        public Task<BookingDto> CreateBooking(BookingDto bookingDto)
+        {
+            var booking = BookingDto.MapToEntity(bookingDto);
+
+            booking.Save(_bookingRepository);
+
+            _bookingRepository.CreateBooking(booking);
+        }
+
+        public Task<BookingDto> GetBooking(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
